34ff82126d05274f47276ec1510a03483defd405
Waldir Borba Junior <wborbajr@gmail.com>
2018-08-15 11:50:05 +0000

    testMail.py created online with Bitbucket

---
+++ testMail.py
@@ -0,0 +1,9 @@
+
+#from SendMail
+import sendmail
+
+email = sendmail.MailSender('alguem.no.gmail@gmail.com','*********','alguem.no.gmail@gmail.com','KKKAKKAKKKA','BODE')
+#status = 
+email.send_email()
+
+#print status
